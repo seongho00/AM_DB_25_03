f1866bcacd382473b61c986dd673337cc104ed4b
seongho00 <tjdwns000927@gmail.com>
2025-03-31 10:30:59 +0900

    SecSql, DBUtil을 활용하여 write, list 수정

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.util.DBUtil;
+import org.example.util.SecSql;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -17,13 +21,13 @@ public class App {
 
             Connection conn = null;
 
-            try { // 여기서 연결하는 이유, 계속적으로
+            try {
                 Class.forName("org.mariadb.jdbc.Driver");
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
 
-            String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+            String url = "jdbc:mariadb://127.0.0.1:3306/AM_DB_25_03?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
 
             try {
                 conn = DriverManager.getConnection(url, "root", "");
@@ -63,87 +67,35 @@ public class App {
             System.out.print("내용 : ");
             String body = sc.nextLine();
 
+            SecSql sql = new SecSql();
 
+            sql.append("INSERT INTO article");
+            sql.append("SET regDate = NOW(),");
+            sql.append("updateDate = NOW(),");
+            sql.append("title = ?,", title);
+            sql.append("`body` = ?;", body);
 
-            PreparedStatement pstmt = null;
+            int id = DBUtil.insert(conn, sql);
 
-            try {
-                String sql = "INSERT INTO article ";
-                sql += "SET regDate = NOW(),";
-                sql += "updateDate = NOW(),";
-                sql += "title = '" + title + "',";
-                sql += "`body`= '" + body + "';";
+            System.out.println(id + "번 글이 생성됨");
 
-                System.out.println(sql);
-
-                pstmt = conn.prepareStatement(sql);
-
-                int affectedRow = pstmt.executeUpdate();
-
-                System.out.println(affectedRow + "열에 적용됨");
-
-            } catch (SQLException e) {
-                System.out.println("에러 2: " + e);
-            } finally {
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
 
         } else if (cmd.equals("article list")) {
             System.out.println("==목록==");
 
-            PreparedStatement pstmt = null;
-            ResultSet rs = null;
-
             List<Article> articles = new ArrayList<>();
 
-            try {
-                String sql = "SELECT *";
-                sql += " FROM article";
-                sql += " ORDER BY id DESC;";
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("ORDER BY id DESC");
 
-                System.out.println(sql);
+            List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
 
-                pstmt = conn.prepareStatement(sql);
-
-                rs = pstmt.executeQuery(sql);
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String regDate = rs.getString("regDate");
-                    String updateDate = rs.getString("updateDate");
-                    String title = rs.getString("title");
-                    String body = rs.getString("body");
-
-                    Article article = new Article(id, regDate, updateDate, title, body);
-
-                    articles.add(article);
-                }
-
-            } catch (SQLException e) {
-                System.out.println("에러 3 : " + e);
-            } finally {
-                try {
-                    if (rs != null && !rs.isClosed()) {
-                        rs.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
+            for (Map<String, Object> articleMap : articleListMap) {
+                articles.add(new Article(articleMap));
             }
+
             if (articles.size() == 0) {
                 System.out.println("게시글이 없습니다");
                 return 0;
